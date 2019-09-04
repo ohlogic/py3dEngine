22e045a40ac812d20d6265e38f76d4881e9b4d9d
Stan S <vfpro777@yahoo.com>
2019-09-04 15:38:42 -0400

    glRenderMode GL_SELECT now works while rotation occurs

--- py3dEngine_test/db_template/db.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-import MySQLdb
-db = MySQLdb.connect(host="127.0.0.1",          # your host, usually localhost
-                     user="",                   # your username
-                     passwd="",                 # your password
-                     db="oasis")                # name of the data base
